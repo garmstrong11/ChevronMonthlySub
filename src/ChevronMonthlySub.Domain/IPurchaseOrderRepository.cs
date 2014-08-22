1939cfc9b315f64547d31bcac9c214881cba8ff6
Gary Armstrong <garmstrong@integracolor.com>
2014-08-22 07:21:52 -0500

    Change ReportData class name to PurchaseOrder

---
+++ src/ChevronMonthlySub.Domain/IPurchaseOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace ChevronMonthlySub.Domain
+{
+	using System.Collections.Generic;
+
+	public interface IPurchaseOrderRepository
+	{
+		IEnumerable<FreightLine> FreightLines { get; }
+		IEnumerable<ProductLine> ProductLines { get; }
+		IEnumerable<PurchaseOrder<ProductLine>> GetProductReports(string InvoiceId);
+		IEnumerable<PurchaseOrder<FreightLine>> GetFreightReports(string InvoiceId);
+	}
+}
