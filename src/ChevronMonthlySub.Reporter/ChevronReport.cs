2f9d99c237eaa38c1056431ccccf20807622dc8d
Gary Armstrong <garmstrong@integracolor.com>
2014-08-29 12:15:56 -0500

    Moved Reporter project into src path

---
+++ src/ChevronMonthlySub.Reporter/ChevronReport.cs
@@ -0,0 +1,69 @@
+﻿namespace ChevronMonthlySub.Reporter
+{
+	using Domain;
+	using FlexCel.Report;
+
+	public class ChevronReport
+	{
+	  private readonly ITemplatePathService _templatePathService;
+    
+    public PurchaseOrder PurchaseOrder { get; set; }
+    public bool IsSummary { get; set; }
+    public string DestinationDirectory { get; set; }
+
+	  public ChevronReport(ITemplatePathService templatePathService)
+	  {
+	    _templatePathService = templatePathService;
+	  }
+
+		public FlexCelReport CreateAndConfigureReport()
+		{
+		  var report = new FlexCelReport(true);
+      report.SetValue("TaxType", PurchaseOrder.TaxType);
+			report.SetValue("PoNumber", PurchaseOrder.PoNumber);
+			report.SetValue("InvoiceNumber", PurchaseOrder.InvoiceNumber);
+			report.SetValue("Recipient", PurchaseOrder.Recipient);
+
+		  var fpo = PurchaseOrder as FreightPurchaseOrder;
+		  if (fpo != null)
+		  {
+        report.AddTable("States", fpo.States);
+        report.SetValue("SubTotal", fpo.LineAmountSubtotal);
+        report.SetValue("TaxTotal", fpo.TaxAmountSubtotal);
+		  }
+
+		  var ppo = PurchaseOrder as ProductPurchaseOrder;
+		  if (ppo == null) return report;
+
+		  report.AddTable("States", ppo.States);
+		  report.SetValue("PickPackTotal", ppo.PickPackCharge);
+		  report.SetValue("BoxTotal", ppo.BoxCharge);
+
+		  return report;
+		}
+
+		public string Prefix
+		{
+		  get { return IsSummary ? "Summary" : "Chevron FG"; }
+		}
+
+	  public string Suffix
+	  {
+	    get { return PurchaseOrder is FreightPurchaseOrder ? " FRT.xlsx" : ".xlsx"; }
+	  }
+
+	  public string GetOutputFilename
+	  {
+	    get
+	    {
+	      return string.Format("{0} {1} {2} {3} {4}{5}",
+	        Prefix,
+	        PurchaseOrder.InvoiceNumber,
+	        PurchaseOrder.PoNumber,
+	        PurchaseOrder.TaxType,
+	        PurchaseOrder.Recipient.Initials,
+	        Suffix);
+	    }
+	  }
+	}
+}
---
+++ src/ChevronMonthlySub.Reporter/FlexcelChevronReportAdapter.cs
@@ -0,0 +1,55 @@
+﻿namespace ChevronMonthlySub.Reporter
+{
+	using System;
+	using System.Collections.Generic;
+	using System.IO;
+	using Domain;
+	using FlexCel.Core;
+	using FlexCel.Report;
+
+  public class FlexcelChevronReportAdapter : IChevronReportAdapter
+  {
+    private readonly FlexCelReport _report;
+	  private readonly ITemplatePathService _templatePathService;
+
+	  public FlexcelChevronReportAdapter(ITemplatePathService templatePathService)
+    {
+	    _templatePathService = templatePathService;
+		  _report = new FlexCelReport(true);
+    }
+    
+    public void AddTable<T>(string tableName, IEnumerable<T> value)
+    {
+      _report.AddTable(tableName, value);
+    }
+
+    public void SetValue(string name, object value)
+    {
+      _report.SetValue(name, value);
+    }
+
+    public void Run(bool isSummary)
+    {
+      _report.SetValue("LastDay", GetLastDayOfThisMonthAsDouble());
+
+			var prefix = isSummary ? "Summary" : "Chevron FG";
+      var outputFilename = string.Format("{0} {1}", prefix, OutputFileNameWithoutPrefix);
+
+	    var outputPath = Path.Combine(_templatePathService.OutputDirectory, outputFilename);
+	    var templatePath = _templatePathService.GetTemplatePath(isSummary, Type);
+
+      _report.Run(templatePath, outputPath);
+    }
+
+	  public Type Type { get; set; }
+
+	  public string OutputFileNameWithoutPrefix { get; set; }
+
+		private static double GetLastDayOfThisMonthAsDouble()
+		{
+			var now = DateTime.Now;
+			var lastDay = new DateTime(now.Year, now.Month, DateTime.DaysInMonth(now.Year, now.Month));
+			return FlxDateTime.ToOADate(lastDay, false);
+		}
+  }
+}
